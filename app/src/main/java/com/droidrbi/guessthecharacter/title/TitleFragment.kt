9c001b54b98008d987807e463c05a70d69a9408e
ipshitaRB <ipshitarburman@gmail.com>
2020-05-29 15:42:45 -0400

    Add binding to title

--- app/src/main/java/com/droidrbi/guessthecharacter/title/TitleFragment.kt
+++ app/src/main/java/com/droidrbi/guessthecharacter/title/TitleFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.droidrbi.guessthecharacter.R
+import com.droidrbi.guessthecharacter.databinding.FragmentTitleBinding
 
 
 /**
@@ -13,13 +15,19 @@ import com.droidrbi.guessthecharacter.R
  */
 class TitleFragment : Fragment() {
 
+    private lateinit var titleBinding: FragmentTitleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        // Inflate the layout for this fragment
+        titleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false
+        )
+
+        return titleBinding.root
     }
 
 

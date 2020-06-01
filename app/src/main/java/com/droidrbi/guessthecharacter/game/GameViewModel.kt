771f1b4cc204f804cf789655afabeabccbed676d
ipshitaRB <ipshitarburman@gmail.com>
2020-06-01 18:04:51 -0400

    Add resetList method

--- app/src/main/java/com/droidrbi/guessthecharacter/game/GameViewModel.kt
+++ app/src/main/java/com/droidrbi/guessthecharacter/game/GameViewModel.kt
@@ -20,4 +20,25 @@ class GameViewModel : ViewModel() {
         super.onCleared()
         Log.i("GameViewModel", "OnCleared called - GameViewModel Destroyed!!!")
     }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        _characterList = mutableListOf(
+            Character("Darth Vader", "I am Luke's father"),
+            Character("Han Solo", "Millenium Falcon is my most prized possession"),
+            Character("Indian Jones", "I can not do without my whip and fedora"),
+            Character("Harry Potter", "I beat Voldemort when I was a baby"),
+            Character(
+                "Ellen Ripley",
+                "I am the sole survivor of the Nostromo spaceship in the movie Alien"
+            ),
+            Character("Mickey Mouse", "Walt Disney created me and I have large ears like a mouse")
+
+        )
+        _characterList.shuffle()
+    }
+
+
 }

c9a8190a1f0bb848da235a34cbc27f7a43c81852
ipshitaRB <ipshitarburman@gmail.com>
2020-05-31 23:14:50 -0400

    Add reference to ViewModel in Fragment

--- app/src/main/java/com/droidrbi/guessthecharacter/game/GameFragment.kt
+++ app/src/main/java/com/droidrbi/guessthecharacter/game/GameFragment.kt
@@ -1,11 +1,12 @@
 package com.droidrbi.guessthecharacter.game
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.droidrbi.guessthecharacter.R
 
 /**
@@ -13,10 +14,15 @@ import com.droidrbi.guessthecharacter.R
  */
 class GameFragment : Fragment() {
 
+    private lateinit var _viewModel: GameViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        _viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        Log.i("GameFragment", "ViewModelProvider called")
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false)
     }

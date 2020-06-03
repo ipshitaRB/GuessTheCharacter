4e858bf14f7b7f0f9ab3bc98d254a4d39c57a284
ipshitaRB <ipshitarburman@gmail.com>
2020-06-02 20:53:25 -0400

    Instantiate binding

--- app/src/main/java/com/droidrbi/guessthecharacter/score/ScoreFragment.kt
+++ app/src/main/java/com/droidrbi/guessthecharacter/score/ScoreFragment.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.droidrbi.guessthecharacter.R
+import com.droidrbi.guessthecharacter.databinding.FragmentScoreBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class ScoreFragment : Fragment() {
 
+    private lateinit var _binding: FragmentScoreBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_score,
+            container,
+            false
+        )
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false)
+        return _binding.root
     }
 
 }

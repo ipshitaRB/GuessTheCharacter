11b3f0401d6447972dc43dd0b30ce8d2f738d9b7
ipshitaRB <ipshitarburman@gmail.com>
2020-06-02 23:51:36 -0400

    Attach observers to LiveData objects

--- app/src/main/java/com/droidrbi/guessthecharacter/game/GameFragment.kt
+++ app/src/main/java/com/droidrbi/guessthecharacter/game/GameFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.droidrbi.guessthecharacter.R
@@ -41,9 +42,14 @@ class GameFragment : Fragment() {
         _binding.submitButton.setOnClickListener { onSubmit() }
         _binding.skipButton.setOnClickListener { onSkip() }
         _binding.endGameButton.setOnClickListener { onEnd() }
+        _viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            _binding.scoreTextView.text = getString(R.string.score, newScore)
+        })
+        _viewModel.currentCharacter.observe(viewLifecycleOwner, Observer { newCharacter ->
 
-        updateCharacterHint()
-        updateScoreText()
+            _binding.characterEditText.text?.clear()
+            _binding.textInputLayout.hint = getString(R.string.hint, newCharacter.hint)
+        })
         // Inflate the layout for this fragment
         return _binding.root
     }
@@ -64,28 +70,16 @@ class GameFragment : Fragment() {
 
     private fun onSkip() {
         _viewModel.onSkip()
-        updateCharacterHint()
     }
 
     private fun onSubmit() {
         if (_viewModel.isCorrect(_binding.characterEditText.text.toString())) {
             _viewModel.onCorrect()
             Toast.makeText(context, getString(R.string.correct_answer), Toast.LENGTH_SHORT).show()
-            updateCharacterHint()
-            updateScoreText()
         } else {
             Toast.makeText(context, getString(R.string.try_again), Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun updateScoreText() {
-        _binding.scoreTextView.text = getString(R.string.score, _viewModel.score.value ?: 0)
-    }
-
-    private fun updateCharacterHint() {
-        _binding.characterEditText.text?.clear()
-        _binding.textInputLayout.hint =
-            getString(R.string.hint, _viewModel.currentCharacter.value?.hint)
-    }
 
 }
